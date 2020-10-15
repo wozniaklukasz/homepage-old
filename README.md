397e03ee7994a7d9cfcfcf6dcc431540db93922b
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2020-10-15 18:20:35 +0200

    init gatsby-starter-solidstate

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Personal website.
 
-Based on [gatsby-simplefolio](https://github.com/cobidev/gatsby-simplefolio).
+Based on [gatsby-starter-solidstate](https://github.com/anubhavsrivastava/gatsby-starter-solidstate).
 
 GH-Pages push `git subtree push --prefix public origin gh-pages`
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,25 @@
+const config = require('./config');
+
 module.exports = {
+  pathPrefix: config.pathPrefix,
+  siteMetadata: {
+    title: config.siteTitle,
+  },
   plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
-    },
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#02aab0`,
-        display: `standalone`,
-        icon: 'src/images/favicon.png',
+        name: config.manifestName,
+        short_name: config.manifestShortName,
+        start_url: config.pathPrefix || config.manifestStartUrl,
+        background_color: config.manifestBackgroundColor,
+        theme_color: config.manifestThemeColor,
+        display: config.manifestDisplay,
+        icon: config.manifestIcon, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
   ],
 };
